6f9c8d6c81e6b1646fbf1637f2c5df76f245b352
Newton Lomar <newtonlomar16@gmail.com>
2022-12-12 23:36:13 -0300

    first commit + setup do prisma + rotas e validação

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { HttpModule } from './http.module';
+import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
+
+@Module({
+  imports: [HttpModule],
+  controllers: [AppController],
+  providers: [PrismaService],
+})
+export class AppModule {}
