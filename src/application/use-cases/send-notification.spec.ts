2ca38b298a7bf4ad3ea2681a5d1bf3e1f4c218de
Newton Lomar <newtonlomar16@gmail.com>
2022-12-14 18:16:10 -0300

    repositório de aplicações (banco de dados dentro da aplicação, para teste sem necessidade de conectar com nenhum banco) + testando caso de uso

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,8 +1,17 @@
+import { Notification } from '../entities/notification';
 import { SendNotification } from './send-notification';
+
+const notifications: Notification[] = [];
+
+const notificationsRepository = {
+  async create(notification: Notification) {
+    notifications.push(notification);
+  },
+};
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationsRepository);
 
     const { notification } = await sendNotification.execute({
       content: 'This is a notification',
@@ -10,6 +19,8 @@ describe('Send notification', () => {
       recipientId: 'example-recipient-id',
     });
 
-    expect(notification).toBeTruthy();
+    console.log(notifications);
+
+    expect(notifications).toHaveLength(1);
   });
 });
